8af0f7cce733eab5e32be82287241a62b2053a21
Anna <ds_anna@bk.ru>
2022-05-12 11:14:49 +0300

    Определить номер четверти плоскости

---
+++ task2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите координаты Х: ");
+int coordX = int.Parse(Console.ReadLine());
+Console.Write("Введите координаты Y: ");
+int coordY = int.Parse(Console.ReadLine());
+if (coordX < 0 && coordY < 0)
+{
+    Console.WriteLine("Координаты вашей точки в 1 четверти плоскости ");
+}
+else if (coordX < 0 && coordY > 0)
+{
+    Console.WriteLine("Координаты вашей точки во 2 четверти плоскости ");
+}
+else if (coordX > 0 && coordY > 0)
+{
+    Console.WriteLine("Координаты вашей точки в 3 четверти плоскости ");
+}
+else if (coordX > 0 && coordY < 0)
+{
+    Console.WriteLine("Координаты вашей точки в 4 четверти плоскости ");
+}
+else
+    Console.WriteLine("Ваша точка вне плоскостей");
+
